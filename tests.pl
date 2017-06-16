4d27f83c7bd67c4acd81237b07288a17d3607eee
Hans <zephyrfalcon@gmail.com>
2017-06-15 23:12:45 -0400

    added first tests

--- tests.pl
+++ tests.pl
@@ -2,9 +2,19 @@
 
 :- consult(miu).
 
-:- begin_tests(miu).
+:- begin_tests(assemble_line).
 
-:- end_tests(miu).
+% dis/assemble rts
+
+test(assemble_line, []) :-
+    assemble_line(rts/implied, Bytes, 0xC000, []),
+    assertion(Bytes = [0x60]).
+
+test(assemble_line, []) :-
+    assemble_line(Instruction/Mode, [0x60], 0xC000, []),
+    assertion(Instruction/Mode = rts/implied).
+
+:- end_tests(assemble_line).
 
 go :-
     run_tests.
